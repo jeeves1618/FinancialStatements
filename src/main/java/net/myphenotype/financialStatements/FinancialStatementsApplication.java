ea4bdbbe443fa206a7fa16fa2395d5d2f89ad865
Vijay Ponnusamy <jeeves1618@gmail.com>
2022-04-28 18:34:36 +0530

    Initial Commit

---
+++ src/main/java/net/myphenotype/financialStatements/FinancialStatementsApplication.java
@@ -0,0 +1,13 @@
+package net.myphenotype.financialStatements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialStatementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialStatementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/myphenotype/financialStatements/FinancialStatementsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.myphenotype.financialStatements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialStatementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
